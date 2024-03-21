1b60ff704ca6189b137acd37066bedae9f0b47dc
Pilag6 <pilag6@gmail.com>
2024-03-21 18:33:12 +0100

    YEAH! We connected the Backend with the Frontend!!!!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+    const [movies, setMovies] = useState({
+        movies: []
+    });
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await Axios.get("http://localhost:300/movies");
+                // console.log(response);
+                setMovies((prev) => ({
+                    ...prev,
+                    movies: response.data
+                }));
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchMovies();
+    }, []);
+
+    console.log(movies.movies);
+
     return (
         <>
-            <h1>Minimalistic React Project</h1>
+            <h1>All the movies here</h1>
+            {movies.movies.map((movie) => {
+                return (
+                    <>
+                        <h2 key={movie._id}>{movie.title}</h2>
+                        <h3>{movie.director}</h3>
+                        <p>Year: {movie.year}</p>
+                        <p>Genre: {movie.genre.join(", ").toUpperCase()}</p>
+                        <img src={movie.cover} alt="" />
+                    </>
+                );
+            })}
         </>
     );
 }
